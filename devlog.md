4c075766ce1a3c1760077893e195e46b352adbc1
hrithick-codes <hrithick.codes@gmail.com>
2025-12-08 01:02:37 +0530

    first commit

---
+++ devlog.md
@@ -0,0 +1,4 @@
+# CURRENT CONTEXT
+STATUS: Folder structure created. Config defined. Config added
+NEXT: Write the tokenizer script in 'prepare_data.py' to download TinyStories.
+      Write the model architecture
---
+++ model/config.py
@@ -0,0 +1,20 @@
+from dataclasses import dataclass
+
+
+@dataclass
+class GPTConfig:
+    block_size: int = 1024
+    vocab_size: int = 50257
+    n_layer: int = 12
+    n_head: int = 12
+    n_embd: int = 768
+    dropout: float = 0.1
+    bias: bool = True
+
+    # -------------------------------------------------------------------------
+    # TinyStories Debug Config
+    # -------------------------------------------------------------------------
+    # block_size: int = 256
+    # n_layer: int = 4
+    # n_head: int = 4
+    # n_embd: int = 128

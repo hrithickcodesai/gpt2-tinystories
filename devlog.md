3b9fa65adbebb76ab27c6c8bd921504d7d80356e
hrithick-codes <hrithick.codes@gmail.com>
2026-01-23 23:42:19 +0530

    training code added

--- devlog.md
+++
@@ -1,4 +0,0 @@
-# CURRENT CONTEXT
-STATUS: GPT2 model implented.
-NEXT: Write the tokenizer script in 'prepare_data.py' to download TinyStories.
-      Write the model architecture
--- train.py
+++ train.py
@@ -8,11 +8,14 @@ from model.config import BabyGPTConfig
 from model.gpt2 import GPT2Model
 from training.dataset import TinyStoriesDataset
 
-BATCH_SIZE = 1
+BATCH_SIZE = 8
 LEARNING_RATE = 3e-4
-MAX_TOKENS = 1000000
-DEVICE = "mps" if torch.backends.mps.is_available() else "cpu"
-print(f"Using device: {DEVICE}")
+WEIGHT_DECAY = 0.1
+MAX_TOKENS = 10000000
+DEVICE = "cuda" if torch.cuda.is_available() else "cpu"
+USE_BFLOAT16 = torch.cuda.is_available() and torch.cuda.is_bf16_supported()
+PT_DTYPE = torch.bfloat16 if USE_BFLOAT16 else torch.float16
+print(f"Using device: {DEVICE} | Dtype: {PT_DTYPE}")
 
 config = BabyGPTConfig()
 
@@ -27,15 +30,27 @@ gpt2 = GPT2Model(
 params = sum([p.numel() for p in gpt2.parameters()])
 print(f"Model parameters: {params / 1e6:.2f}M")
 
+print("Moving model to device and compiling...")
 gpt2.to(DEVICE)
-optimizer = torch.optim.AdamW(gpt2.parameters(), lr=LEARNING_RATE)
+gpt2 = torch.compile(gpt2)
 
+optimizer = torch.optim.AdamW(
+    gpt2.parameters(), lr=LEARNING_RATE, weight_decay=WEIGHT_DECAY
+)
+
+
+scaler = torch.cuda.amp.GradScaler(enabled=(PT_DTYPE == torch.float16))
 
 tiny_dataset = TinyStoriesDataset(
     data_path="data/train_473992236tkns.bin", block_size=config.block_size
 )
 train_dataloader = DataLoader(tiny_dataset, batch_size=BATCH_SIZE, shuffle=True)
 
+
+val_tiny_dataset = TinyStoriesDataset(
+    data_path="data/val_4765918tkns.bin", block_size=config.block_size
+)
+val_dataloader = DataLoader(val_tiny_dataset, batch_size=BATCH_SIZE, shuffle=False)
 
 gpt2.train()
 
@@ -50,22 +65,25 @@ while tokens_seen < MAX_TOKENS:
 
         x, y = x.to(DEVICE), y.to(DEVICE)
 
-        logits = gpt2(x)
-        step += 1
+        with torch.autocast(device_type=DEVICE, dtype=PT_DTYPE):
+            logits = gpt2(x)
+            B, T, C = logits.shape
+            loss = F.cross_entropy(logits.view(B * T, C), y.view(B * T))
 
-        B, T, C = logits.shape
-        loss = F.cross_entropy(logits.view(B * T, C), y.view(B * T))
         perplexity = torch.exp(loss)
-
         optimizer.zero_grad()
-        loss.backward()
-        optimizer.step()
+        scaler.scale(loss).backward()
+        scaler.step(optimizer)
+        torch.nn.utils.clip_grad_norm_(gpt2.parameters(), max_norm=1.0)
+        scaler.update()
 
         tokens_in_batch = B * T
         tokens_seen += tokens_in_batch
         end_time = time.time()
         dt = end_time - start_time
         tok_per_sec = tokens_in_batch / dt
+        step += 1
+
         print(
             f"Step {step} | "
             f"Tokens: {tokens_seen} | "
@@ -73,3 +91,8 @@ while tokens_seen < MAX_TOKENS:
             f"Perplexity: {perplexity.item():.4f} | "
             f"Speed: {tok_per_sec:.2f} tok/sec"
         )
+        torch.cuda.synchronize()
+
+
+torch.save(gpt2.state_dict(), f"checkpoints/baby_gpt2_{tokens_seen}.pth")
+print(f"Model checkpoint saved as checkpoints/baby_gpt2_{tokens_seen}.pth")
